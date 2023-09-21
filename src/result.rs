69988f4e034f849a51f224fd306009d7accb5243
Andrew Kane <andrew@ankane.org>
2023-09-21 09:53:55 -0700

    DRY strength

--- src/result.rs
+++ src/result.rs
@@ -24,13 +24,16 @@ impl StlResult {
     }
 
     pub fn seasonal_strength(&self) -> f32 {
-        let sr = self.seasonal().iter().zip(self.remainder()).map(|(a, b)| a + b).collect::<Vec<f32>>();
-        (1.0 - self.var(self.remainder()) / self.var(&sr)).max(0.0)
+        self.strength(self.seasonal())
     }
 
     pub fn trend_strength(&self) -> f32 {
-        let tr = self.trend().iter().zip(self.remainder()).map(|(a, b)| a + b).collect::<Vec<f32>>();
-        (1.0 - self.var(self.remainder()) / self.var(&tr)).max(0.0)
+        self.strength(self.trend())
+    }
+
+    fn strength(&self, component: &[f32]) -> f32 {
+        let sr = component.iter().zip(self.remainder()).map(|(a, b)| a + b).collect::<Vec<f32>>();
+        (1.0 - self.var(self.remainder()) / self.var(&sr)).max(0.0)
     }
 
     fn var(&self, series: &[f32]) -> f32 {
