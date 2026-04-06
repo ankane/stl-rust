2a5979230bd6bec53c8915de8710a09b5acb8aa3
Andrew Kane <andrew@ankane.org>
2026-04-05 17:26:55 -0700

    Switched to write_str

--- src/error.rs
+++ src/error.rs
@@ -20,16 +20,16 @@ impl error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Error::Series => write!(f, "series has less than two periods"),
-            Error::SeasonalDegree => write!(f, "seasonal_degree must be 0 or 1"),
-            Error::TrendDegree => write!(f, "trend_degree must be 0 or 1"),
-            Error::LowPassDegree => write!(f, "low_pass_degree must be 0 or 1"),
-            Error::Period => write!(f, "period must be at least 2"),
-            Error::Lambda => write!(f, "lambda must be between 0 and 1"),
+            Error::Series => f.write_str("series has less than two periods"),
+            Error::SeasonalDegree => f.write_str("seasonal_degree must be 0 or 1"),
+            Error::TrendDegree => f.write_str("trend_degree must be 0 or 1"),
+            Error::LowPassDegree => f.write_str("low_pass_degree must be 0 or 1"),
+            Error::Period => f.write_str("period must be at least 2"),
+            Error::Lambda => f.write_str("lambda must be between 0 and 1"),
             Error::SeasonalLengths => {
-                write!(f, "seasonal_lengths must have the same length as periods")
+                f.write_str("seasonal_lengths must have the same length as periods")
             }
-            Error::EmptyPeriods => write!(f, "periods must not be empty"),
+            Error::EmptyPeriods => f.write_str("periods must not be empty"),
         }
     }
 }
